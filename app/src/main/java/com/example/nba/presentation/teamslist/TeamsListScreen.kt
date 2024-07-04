72844ad66c1815a81c6ab8df46e2cc8b733b6a62
SILVAG0D <liesch576@gmail.com>
2024-07-03 23:43:50 -0300

    Comeca tela de lista de times

--- app/src/main/java/com/example/nba/presentation/teamslist/TeamsListScreen.kt
+++ app/src/main/java/com/example/nba/presentation/teamslist/TeamsListScreen.kt
@@ -1,11 +1,66 @@
 package com.example.nba.presentation.teamslist
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.nba.R
+
+// Sample data
+val teams = listOf(
+    "Lakers",
+    "Warriors",
+    "Nets",
+    "Bucks",
+    "Clippers"
+)
 
 @Composable
-fun TeamsListScreen(
+fun TeamsListScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = Color.LightGray).padding(8.dp)
+    ) {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            items(teams) { team ->
+                TeamItem(teamName = team)
+            }
+        }
+    }
+}
 
-){
-    Text(text ="LISTA DE TIMES")
+@Composable
+fun TeamItem(teamName: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp),
+        onClick = {
+            // Handle item click here
+        }
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp).align(Alignment.CenterHorizontally),
+            horizontalArrangement = Arrangement.SpaceBetween,
+
+        ) {
+            Text(text = teamName)
+            Icon(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground), // replace with your icon resource
+                contentDescription = null
+            )
+        }
+    }
 }
