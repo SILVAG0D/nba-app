6faf7323e283c627c5e494d1724d24b35591719f
SILVAG0D <liesch576@gmail.com>
2024-09-01 11:42:32 -0300

    cria camada de injeção de dependência.

---
+++ app/src/main/java/com/example/nba/di/NbaApplication.kt
@@ -0,0 +1,9 @@
+package com.example.nba.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class NbaApplication : Application() {
+}
---
+++ app/src/main/java/com/example/nba/di/NbaModule.kt
@@ -0,0 +1,7 @@
+package com.example.nba.di
+
+import dagger.Module
+
+@Module
+abstract class NbaModule {
+}
